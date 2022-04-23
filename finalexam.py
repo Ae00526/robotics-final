8feecfbf4e573b7fe10077e6faf72eae94369191
ahmedbassem03 <85519423+ahmedbassem03@users.noreply.github.com>
2022-04-23 12:49:15 +0200

    Update finalexam.py
    Added quality of life changes, such as user option to show data for distance and angle to the target and obstacles, and option to have a delay or not so the user can have more control. Refined the code and fixed grammar

--- finalexam.py
+++ finalexam.py
@@ -1,3 +1,4 @@
+
 from roboticstoolbox import Bicycle, RandomPath, VehicleIcon, RangeBearingSensor, LandmarkMap
 from math import pi, atan2, sqrt
 import matplotlib.pyplot as plt
@@ -6,11 +7,17 @@ from scipy.io import loadmat
 import numpy as np
 from time import sleep
 
-startx= float(input("choose X coordinates for the robot: ")) #coordinates of the robot
-starty= float(input("choose Y coordinates for the robot: "))
-targetx= float(input("choose your targets X coordinates: ")) #coordinates of the target
-targety= float(input("choose your targets Y coordinates: "))
-obstaclenum= int(input("choose number of obstacles in the map: ")) #number of obstacles randomly generated 
+startx= float(input("Choose X coordinates for the robot: ")) #coordinates of the robot
+starty= float(input("Choose Y coordinates for the robot: "))
+targetx= float(input("Choose your targets X coordinates: ")) #coordinates of the target
+targety= float(input("Choose your targets Y coordinates: "))
+obstaclenum= int(input("Choose number of obstacles in the map: ")) #number of obstacles randomly generated 
+targdis=str(input("Do you want to see the distance and angle to the target? \'yes' or \'no': ")) #user choice to display constant change in angle and distance to set target
+obstdis=str(input("Do you want to see the distance and angle to each obstacle? \'yes' or \'no': ")) #user choice to see distance and angle to each obstacle, incase theres alot
+delay=str(input("Do you want a delay in the data displayed? \'yes' or \'no': ")) #user choice for a delay in data output and movement of robot
+targdislower=targdis.lower() #no matter how 'yes' is written it'll always activate the condition
+obstdislower=obstdis.lower() #no matter how 'yes' is written it'll always activate the condition
+delaylower=delay.lower() #no matter how 'yes' is written it'll always activate the condition
 
 start=[startx,starty] #making a list of the coordinates of the robot
 target=[targetx, targety] #making a list of the coordinates of the target
@@ -33,7 +40,7 @@ plt.plot()
 
 
 
-target_marker_style={ #design of the target
+target_marker_style={ #dictionary for the design of the target
     'marker':'D',
     'markersize': 7,
     'color': 'b'
@@ -57,7 +64,8 @@ run = True #move robot towards target
 #                 veh._animation.update(veh.x)
 #                 plt.pause(0.005)
 while (run):
-    sleep(0.2) #slight delay so output distances arent spammed in terminal, does slow down movement of robot
+    if delaylower== 'yes':
+        sleep(0.2) #slight delay so output distances arent spammed in terminal, does slow down movement of robot
     goal_heading=atan2(
         target[1]-veh.x[1], #calculating distance to target using TAN and the distance
         target[0]-veh.x[0]
@@ -65,12 +73,15 @@ while (run):
     steer=goal_heading-veh.x[2]
     
     
-    veh.step(3,steer)#speed of the robot moving and the angle to target
-    
-    print("angle to target: ",(steer*180)/pi, "degrees",  end="...") #changing the angle from radian to degrees
-    print("distance to target: ",(abs(sqrt((targetx-veh.x[0])**2+(targety-veh.x[1])**2))), "metres") #distance to target using pythagorean theorem
-    sens=sensor.h(veh.x) 
-    print("distance and angle to obstacles \n", sens) #\n to make the data show below the text
+    veh.step(3,steer) #speed of the robot moving and the angle to target
+    if targdislower=='yes':
+        print("Distance to target: ",(abs(sqrt((targetx-veh.x[0])**2+(targety-veh.x[1])**2))), "metres", end="...") #distance to target using pythagorean theorem
+        print("Angle to target: ",(steer*180)/pi, "degrees") #changing the angle from radian to degrees
+        
+    sens=sensor.h(veh.x)
+    if obstdislower=='yes': 
+        print("Distance and Angle to obstacles \n", sens) #\n to make the data show below the text
+   
     # for i in sens: 
     #     print("distance to obstacles", i[0]) #distance to randomly generated obstacles
     #     print("angle to obstacles: ",i[1])
@@ -82,6 +93,7 @@ while (run):
        
     else:
         run=False
+        print("Target reached")
         break
 
     for i in sens:
@@ -91,7 +103,7 @@ while (run):
         elif i[0]<2 and i[1]<0:
             veh.x[2]+=pi/4
             
-    veh._animation.update(veh.x)
+    veh._animation.update(veh.x) #update vehicles animtion
     
     # if avoidance(sensor.h(veh.x))==false:
     #     run=false
